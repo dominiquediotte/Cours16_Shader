d289a21f5266b19ddc147ee428c508400fc221ad
Dominique Diotte <dominique.diotte@icloud.com>
2020-11-12 22:49:20 -0500

    Post-processing shader created

---
+++ Assets/CameraEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour
+{
+    private Material m_Material;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_Material = new Material(Shader.Find("Hidden/Red"));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, null, m_Material);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
